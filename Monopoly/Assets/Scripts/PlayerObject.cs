05f56eae8c405ccf3778ae3eb8c08ee71ad984ed
Text-Input <hintondavid11@gmail.com>
2017-05-23 16:21:09 -0400

    start of player and property classes

---
+++ Monopoly/Assets/Scripts/PlayerObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerObject /*: MonoBehaviour*/ {
+
+    int square;
+    int money;
+    List<string> ownedProperty = new List<string>();
+    List<string> morgagedProperty = new List<string>();
+
+
+
+    PlayerObject() {
+
+    }
+    public class property {
+        string name;
+        int colorGroup;
+        int buyPrice;
+        int morgagePrice;
+
+        string utilitytype;
+        int rent0;  // rent with nothing on it
+        int rent1;
+        int rent2;
+        int rent3;
+        int rent4;  //
+        int rent5;  // rent with a hotel
+
+
+        property(string name,int Group, int buyPrice, int morgagePrice,string utilitytype, int rent) {
+
+            this.name = name;
+            this.colorGroup = Group;
+
+
+        }
+    }
+}
