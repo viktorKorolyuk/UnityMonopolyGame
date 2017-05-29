f5c1788043642ba0411203708180b9cc137a6274
Viktor Korolyuk <viktor.korolyuk@gmail.com>
2017-05-29 14:06:07 -0400

    Adding pdf for ease of access

---
+++ Monopoly/Assets/Scripts/GameController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	GameObject[] PlayerGroup; //Collection of players
+	int playerTurn = 0;
+
+
+	void init(){
+		
+	}
+	void diceRoll(){
+		//Here we will simulate two dice rolls.
+	}
+}
